837156b96878d17ec8dba613de827d4102ca0a28
Yuya Soneoka <ej.a9p96@gmail.com>
2017-05-20 01:39:34 +0900

    Make  string longer

--- airesearch/models/company.py
+++ airesearch/models/company.py
@@ -6,7 +6,7 @@ class Company(Base):
     __tablename__ = 'companies'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(32), nullable=False, index=True, unique=True)
+    name = Column(String(128), nullable=False, index=True, unique=True)
     crunchbase = Column(String(256), index=True)
     url = Column(String(256), index=True)
     abstract = Column(Text)
@@ -14,11 +14,11 @@ class Company(Base):
     original_description = Column(Text)
     japanese_description = Column(Text)
     founded_date = Column(Date)
-    role = Column(String(32))
+    role = Column(String(256))
     categories = Column(String(256))
     news_page = Column(String(256))
-    place = Column(String(32))
-    funding = Column(String(32))
+    place = Column(String(256))
+    funding = Column(String(256))
 
     def __repr__(self):
         return "<Company(id={0}, name={1}>".format(self.id, self.name)
