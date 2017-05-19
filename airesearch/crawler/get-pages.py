33bf76e54c22e65a6d2269dc04e2d2901730c219
Yuya Soneoka <ej.a9p96@gmail.com>
2017-05-19 14:53:34 +0900

    Change directry name

--- airesearch/crawler/get-pages.py
+++ airesearch/crawler/get-pages.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import airesearch
 
 driver = webdriver.Firefox()
 driver.get("http://www.python.org")
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
 setup(
-  name="ai_research",
+  name="airesearch",
   packages=find_packages()
 )
